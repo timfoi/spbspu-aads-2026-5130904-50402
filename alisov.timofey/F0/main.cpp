0646423cd7dc8153478dc8ef67f79be31fc306a8
m0trex <timofeyalisov@gmail.com>
2026-06-22 23:27:17 +0300

    initial struct

--- alisov.timofey/F0/main.cpp
+++ alisov.timofey/F0/main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <vector>
+
+template <typename K, typename V>
+struct B{
+    K key;
+    V value;
+    int psl = -1;
+};
+
+template <typename K, typename V>
+struct RobinHood{
+    std::vector<B<K,V>> tab;
+};
+
 
 int main(){
-    
+
 }
