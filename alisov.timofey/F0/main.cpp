9255a620ffe9de088dc42ca7afa696394e23e82a
m0trex <timofeyalisov@gmail.com>
2026-06-30 00:00:27 +0300

    Base structures

--- alisov.timofey/F0/main.cpp
+++ alisov.timofey/F0/main.cpp
@@ -1,19 +1,34 @@
+#include <algorithm>
+#include <cstddef>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 #include <vector>
 
-template <typename K, typename V>
-struct B{
-    K key;
-    V value;
-    int psl = -1;
+enum class BucketState { Empty, Occupied, Deleted };
+
+struct Mail
+{
+  std::string track_id;
+  std::string current_office;
+  std::string current_post;
+  double weight = 0.0;
 };
 
-template <typename K, typename V>
-struct RobinHood{
-    std::vector<B<K,V>> tab;
+struct Link
+{
+  std::string to_office;
+  double distance = 0.0;
+  double cost = 0.0;
+  double time = 0.0;
 };
 
+struct Office
+{
+  std::string name;
+  std::vector< Link > links;
+  std::vector< std::string > local_mail_ids;
+};
 
-int main(){
-
-}
+int main()
+{}
