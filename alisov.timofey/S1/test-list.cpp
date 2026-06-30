0699e8b92dc5a00bb3a515e25620cb6b5d2daa0d
m0trex <timofeyalisov@gmail.com>
2026-06-30 17:47:18 +0300

    lil fixes

--- alisov.timofey/S1/test-list.cpp
+++ alisov.timofey/S1/test-list.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_MODULE S1
 #include <stdexcept>
-#include <boost/test/included/unit_test.hpp>
-#include "list.hpp"
+#include <boost/test/unit_test.hpp>
+#include "../common/list.hpp"
 
 BOOST_AUTO_TEST_CASE(default_constructor)
 {
